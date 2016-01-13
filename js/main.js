06d5687df7afb1b371c67e66e506317be7421276
Jason Vangelisti <jvangelisti@synlawn.com>
2016-01-12 21:25:07 -0800

    dropdown of common items added

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$('#add-button').click(function() {
-	$('ul').append('<li class="item">' + $('input#add-item').val() + '</li>' + '<button class="remove">Remove</button>');
-	});
+    $('#add-button').click(function() {
+        $('ul').append('<li class="item">' + $('input#add-item').val() + '</li>' + '<button class="remove">Remove</button>');
+    });
 
-	$('#remove-button').click(function() {
-		alert("clicked");
-	$('ul').remove('<li class="item">' - $('input#add-item').val() - '</li>' - '<button class="remove">Remove</button>');
-	});
+    $('#add-select-button').click(function() {
+        $('ul').append('<li class="item">' + $('select#add-item').val() + '</li>' + '<button class="remove">Remove</button>');
+    });
+
+    // $('#remove-button').click(function() {
+    //     alert("clicked");
+    //     $('ul').remove('<li class="item">' - $('input#add-item').val() - '</li>' - '<button class="remove">Remove</button>');
+
+
+
+    // });
+
+
+    $("ul").on("click", ".remove", function() {
+        $(this).prev(".item").remove();
+        $(this).remove();
+
+    })
+
 });
